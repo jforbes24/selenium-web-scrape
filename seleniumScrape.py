0559746bb9ae55a8ea3bcd928c5ace3bcdf21caf
jforbes24 <64039648+jforbes24@users.noreply.github.com>
2020-11-08 17:44:54 +0000

    Add files via upload

--- seleniumScrape.py
+++ seleniumScrape.py
@@ -22,13 +22,13 @@ user_agent_list = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
     ]
 
+
 for x in range(1,6):
-    for i in range(1,6):
-        # pick a random user agent
-        user_agent = random.choice(user_agent_list)
-        # set the headers
-        headers = {'User-Agent' : user_agent}
-        result = requests.get(f"https://www.diy.com/departments/flooring-tiling/flooring-underlay/laminate-flooring/DIY566433.cat?page={x}", headers=headers)
+    # pick a random user agent
+    user_agent = random.choice(user_agent_list)
+    # set the headers
+    headers = {'User-Agent' : user_agent}
+    result = requests.get(f"https://www.diy.com/departments/flooring-tiling/flooring-underlay/laminate-flooring/DIY566433.cat?page={x}", headers=headers)
 
 # response status
 print(result.status_code)
@@ -39,78 +39,70 @@ chrome_options = Options()
 chrome_options.add_argument(" - incognito")
 driver = webdriver.Chrome(path, options=chrome_options)
 
+productInfo = []
 
-# select url link
-url = "https://www.diy.com/departments/flooring-tiling/flooring-underlay/laminate-flooring/DIY566433.cat?page={x}"
-driver.get(url)
+for x in range(1,6):
+    # select url link
+    url = "https://www.diy.com/departments/flooring-tiling/flooring-underlay/laminate-flooring/DIY566433.cat?page="
+    driver.get(url+str(x))
 
-# accept cookies
-WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@title="TrustArc Cookie Consent Manager"]')))
-WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[@class='call'][text()='Accept Cookies']"))).click()
+    try:
+        # accept cookies
+        WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@title="TrustArc Cookie Consent Manager"]')))
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//a[@class='call'][text()='Accept Cookies']"))).click()
 
-driver.switch_to.default_content()
+        driver.switch_to.default_content()
+    except:
+        break
+    
+    time.sleep(5)
 
-time.sleep(5)
+    # baseurl = 'https://www.diy.com/'
 
-# list criteria
-products = [] # list to store name of the product
-links = [] # list to store link of the product
-prices = [] # list to store price of the product
-unitPrices = [] # list to store unit price of the product
-ratings = [] # list to store rating of the product
-eans = [] # list to store EAN of the product
-reviewNums = [] # list to store number of product reviews
-reviews = [] # list to store reviews of product
-availability = []
+def scrape():
+    flooring = driver.find_elements_by_css_selector('.b9bdc658')
 
-page=2
+    # get product info
+    for f in flooring:
+        title = f.find_element_by_css_selector('p').text
+        testLink = f.find_elements_by_tag_name('a')
+        l = []
+        for link in testLink:
+            l.append(link.get_attribute('href'))
+        """ link = f.find_elements_by_tag_name('a')
+        for l in link:
+            products.append(l.get_attribute('href'))"""
+        price = f.find_element_by_xpath('.//div[@class="b25ad5d5 _4e80f7be _23ee746f _7b343263"]').text
+        unitPrice = f.find_element_by_xpath('.//div[@class="b00398fe b1bfb616"]').text
+        rating = f.find_element_by_xpath('.//div[@class="_45e852d0 _6418d197 _2263bdd0"]').text
+        reviewNum = f.find_element_by_xpath('.//span[@class="ccb9d67a _17d3fa36 _50344329 b1bfb616 cc6bbaee"]').text
 
-# baseurl = 'https://www.diy.com/'
+        products = {
+            'title': title,
+            # 'testLink': testLink,
+            'link': l,
+            'price': price,
+            'unitPrice': unitPrice,
+            'rating': rating,
+            'reviewNum': reviewNum
+        }
 
-flooring = driver.find_elements_by_css_selector('.b9bdc658')
+        productInfo.append(products)
 
-# get product info
-for f in flooring:
-    title = f.find_element_by_css_selector('p').text
-    link = f.find_elements_by_tag_name('a')
-    for l in link:
-        links.append(l.get_attribute('href'))
-    price = f.find_element_by_xpath('.//div[@class="b25ad5d5 _4e80f7be _23ee746f _7b343263"]').text
-    unitPrice = f.find_element_by_xpath('.//div[@class="b00398fe b1bfb616"]').text
-    rating = f.find_element_by_xpath('.//div[@class="_45e852d0 _6418d197 _2263bdd0"]').text
-    reviewNum = f.find_element_by_xpath('.//span[@class="ccb9d67a _17d3fa36 _50344329 b1bfb616 cc6bbaee"]').text
-    products.append(title)
-    prices.append(price)
-    unitPrices.append(unitPrice)
-    ratings.append(rating)
-    reviewNums.append(reviewNum)
+# call scrape function
+scrape()
 
-# ean
-# reviews
-# available
-# print(products, links, prices, unitPrices, reviewNums, ratings)
+# return length of results
+print(len(productInfo))
 
-# compile dictionary of metrics
-d = {'Products':products,
-     'URL':links,
-     'Prices':prices,
-     'Unit_Prices':unitPrices,
-     'Review_qty':reviewNums,
-     }
+# close browser
+driver.close()
 
-print(len(links))
-"""
 # create dataframe
-df = pd.DataFrame(d)
+df = pd.DataFrame(productInfo)
 pd.set_option('display.max_columns', 100)
 
-print(df)
-# click through more product pages
-
-# driver.find_element_by_xpath('//span[@class="e433fa60 f9f4120c').click()
-
-"""
-driver.close()    
-
-
-
+# save to excel
+df.to_excel('floor_lights.xlsx', index=True, header=True)
+data = pd.read_excel('/Users/jforbes84/PycharmProjects/floor_lights.xlsx')
+print(data)
